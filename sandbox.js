f6d479bff05721175403c40d0ea22f46aee94de0
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-04-27 12:51:38 +0600

    learnt type basics

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+var person = 'mario';
+var age = 30;
+var isBlackBelt = true;
+// person = 20;
+person = 'luigi';
+// age = 'mario';
+age = 20;
+// isBlackBelt = 100;
+isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circ('hello'));
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,19 @@
+let person = 'mario';
+let age = 30;
+let isBlackBelt = true;
+
+// person = 20;
+person = 'luigi';
+
+// age = 'mario';
+age = 20;
+
+// isBlackBelt = 100;
+isBlackBelt = false;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+
+// console.log(circ('hello'));
+console.log(circ(7.5));
