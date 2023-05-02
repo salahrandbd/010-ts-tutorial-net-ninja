37699f9ff4ebf553fe7ef928e1f55d7112cb5e4d
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-05-02 14:11:41 +0600

    learnt basics of interface

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,23 @@
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 import { Invoice } from "./classes/invoice.js";
+;
+const person = {
+    name: 'yoshi',
+    age: 20,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    }
+};
+let greeting;
+greeting = (p) => {
+    console.log(p.name, ' is ', p.age, ' years old!');
+};
+greeting(person);
 const invOne = new Invoice('yoshi', 'portfolio website', 200);
 const invTwo = new Invoice('luigi', 'digital marketing', 300);
 const invoices = [];
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,6 +2,33 @@
 // console.log(anchor.href);
 import { Invoice } from "./classes/invoice.js";
 
+interface Person {
+  name: string,
+  age: number,
+  speak(text: string): void,
+  spend(amount: number): number
+};
+
+const person: Person = {
+  name: 'yoshi',
+  age: 20,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  }
+}
+
+let greeting: (a: Person) => void;
+
+greeting = (p: Person): void => {
+  console.log(p.name, ' is ', p.age, ' years old!');
+}
+
+greeting(person);
+
 const invOne = new Invoice('yoshi', 'portfolio website', 200);
 const invTwo = new Invoice('luigi', 'digital marketing', 300);
 
