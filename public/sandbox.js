8bcd39a6c7746befde75085c9ff8258504f215a9
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-05-02 11:38:38 +0600

    learnt basics of classes

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,21 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        console.log(`${this.client} owes $${this.amount} for ${this.details}`);
+    }
+}
+const invOne = new Invoice('yoshi', 'portfolio website', 200);
+const invTwo = new Invoice('luigi', 'digital marketing', 300);
+const invoices = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 const type = document.querySelector('#type');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,30 @@
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(client: string, details: string, amount: number) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    console.log(`${this.client} owes $${this.amount} for ${this.details}`);
+  }
+}
+
+const invOne = new Invoice('yoshi', 'portfolio website', 200);
+const invTwo = new Invoice('luigi', 'digital marketing', 300);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne, invTwo);
+
+console.log(invoices);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
 
