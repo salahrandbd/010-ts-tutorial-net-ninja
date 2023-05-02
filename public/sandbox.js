62afa6178390afb138c2fa5cd979ce1e96f6733a
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-05-02 11:06:16 +0600

    learnt function signature

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,22 @@
 "use strict";
-let getUser = (uuid) => {
-    return { name: 'Salah', uuid };
+// function signature
+// example 01
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+// example 02
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action == 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+// example 03
+let logDetails;
+logDetails = (obj) => {
+    console.log(`${obj.name} is ${obj.age} years old!`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,7 +1,23 @@
-// type aliases
-type StringOrNum = string|number;
-type ObjectWithName = { name: string, uuid: StringOrNum };
+// function signature
 
-let getUser = (uuid: StringOrNum): ObjectWithName => {
-  return { name: 'Salah', uuid };
+// example 01
+let greet: (a: string, b: string) => void 
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
+
+// example 02
+let calc: (a: number, b: number, c: string) => number 
+calc = (numOne: number, numTwo: number, action: string) => {
+  if(action == 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+// example 03
+let logDetails: (obj: { name: string, age: number }) => void 
+logDetails = (obj: { name: string, age: number }) => {
+  console.log(`${obj.name} is ${obj.age} years old!`);
 }
